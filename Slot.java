a452e659d65658014ba44335d3914d1a09348b93
Lexie for OS <lexieforos@lexie.local>
2016-11-03 22:58:20 -0400

    implemented potential data storage for tiles and slots, feel free to obliterate

--- Slot.java
+++ Slot.java
@@ -1,21 +1,61 @@
 public class Slot{
 
-  int meeplePlacement;
+  static int NUM_SIDES = 4;
 
-  private Orientation o;
+  private int meeplePlacement;
+  protected Slot[] adjacent;
+  protected char[] sideTypes;
+
+  //1 = top, 2 = right, -1 = bottom, -2 = left;
+  public Slot(Slot[] sides, int[] whichSide){
+
+    adjacent = new Slot[NUM_SIDES];
+    sideTypes = new char[NUM_SIDES];
+
+    for(int i=0; i<sides.length; i++){
+      connect(sides[i],whichSide[i]);
+    }
+
+  }
+
+  private void connect(Slot s, int dir){
+    this.adjacent[dir] = s;
+    s.adjacent[-dir] = this;
+    this.sideTypes[dir] = s.sideTypes[-dir];
+  }
 
   //borders
 
   private Tile t;
 
-  public boolean canFit(Tile t, Orientation o){
-    //if this tile can go here
-    return true; // for now
+  public boolean canFit(Tile t, int rotationAmt){
+
+    for(int i = 0; i<NUM_SIDES; i++){
+
+      if(rotationAmt+i == NUM_SIDES){
+        rotationAmt = -i;
+      }
+
+      if(sideTypes[i] != t.getSide(rotationAmt+i)){
+        return false;
+      }
+
+    }
+
+    return true;
   }
 
-  public void setTile(Tile t, Orientation o){
+  public void setTile(Tile t, int rotationAmt){
     this.t = t;
-    this.o = o;
+    for(int i = 0; i<NUM_SIDES; i++){
+
+      if(rotationAmt+i == NUM_SIDES){
+        rotationAmt = -i;
+      }
+
+      sideTypes[i] = t.getSide(rotationAmt+i);
+
+    }
   }
 
   public void placeMeeple(int meeplePlacement){
--- Tile.java
+++ Tile.java
@@ -1,3 +1,8 @@
 public class Tile {
-    
+    private char centerType;
+    private char[] sideTypes;
+
+    public char getSide(int i){
+      return sideTypes[i];
+    }
 }
