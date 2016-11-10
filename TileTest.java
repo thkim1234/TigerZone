7889430cebd5568d20cf150701d1e46a26428c92
Michael Pabon <michael@Michaels-MacBook-Pro-2.local>
2016-11-10 18:42:58 -0500

    Testing TileTest

---
+++ TileTest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+/**
+ * Created by michael on 11/10/16.
+ */
+public class TileTest {
+    private Tile tile;
+    @Before
+    public void setUp() throws Exception { tile = new Tile(); }
+
+    @Test
+    public void testConstructor(){
+        assertEquals('g', tile.getCenterType());
+        assertEquals('0', tile.getShieldLoc());
+        char[] sideTypes = tile.getSideTypes();
+        for(int i=0; i<Slot.NUM_SIDES; i++){
+            assertEquals(sideTypes[i], 'g');
+        }
+    }
+}
