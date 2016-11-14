9529494b49c63ce4856707e95b0789c24e781e91
Lexie <lexkrehbiel@gmail.com>
2016-11-14 12:48:50 -0500

    paused work

--- Region.java
+++ Region.java
@@ -1,3 +1,21 @@
 public abstract class Region{
+
+  private HashMap<Player,Integer> meeplesByPlayer;
+
   public abstract void absorb(Region otherRegion);
+
+  public void addMeeple(Player owner){
+    if(!meeplesByPlayer.containsKey(owner)){
+      meeplesByPlayer.put(owner,1);
+    } else {
+      meeplesByPlayer.get(owner)++;
+    }
+  }
+
+  public abstract int totalScore();
+
+
+  public int scoreByPlayer(){
+
+  }
 }
