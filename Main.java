2a2869b2d180bc80f175f9e4a5ceb7a7c4be10e4
Michael Pabon <michael@Michaels-MBP-2.attlocal.net>
2016-11-25 19:30:15 -0500

    Set up netowrking client skeleton in Main. Note: We can use command line args like the knock knock tutorial or an ide it doesn't matter, for not I omitted the use of command line args but we can go back

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.io.*;
+import java.net.*;
+
 public class Main {
     public static void main(String args[]) {
+        /*
         Game game = new Game(new HumanPlayer(), new HumanPlayer());
         GameVisualization printGame = new GameVisualization(game);
 
@@ -9,6 +13,38 @@ public class Main {
         printGame.printTileDeck();
         printGame.printBoard();
         printGame.printPlayerTurn();
+        */
+
+        String hostName = ""; //Given to us by Dave I'm assuming?
+        int portNumber = 0; //Given to us by Dave I'm assuming?
+
+        try (
+                Socket socket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ) {
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+
+            while ((fromServer = in.readLine()) != null) {
+                //What the server outputs
+                System.out.println("Server: " + fromServer);
+                //What we input
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser); //What we typed
+                    out.println(fromUser);  //Send what we typed to server
+                }
+            }
+
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
 
     };
 }
